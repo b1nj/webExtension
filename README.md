6e7ace2e8ecfdf29898e01f018b1a2ff9090dcac
B1nj <postmaster@b1nj.fr>
2017-10-11 16:05:57 -0400

    nemo 1.1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # webExtension
 Mes extensions firefox
+
+Les extensions sont disponible sur Firefox à cette adresse : https://addons.mozilla.org/fr/firefox/user/B1nj/
+
+## Debugging
+```
+web-ext run
+web-ext lint
+web-ext build
+```
+```
+about:debugging
+```
--- nemoWeb/content.js
+++ nemoWeb/content.js
@@ -12,12 +12,49 @@ document.getElementById('header_informations').appendChild(document.getElementBy
 
 /* Ajoute un lien vers l'accueil */
 
-document.getElementById('advanced_search').parentNode.insertAdjacentHTML('beforebegin', '<b><a href="/">Accueil</a></b> ');
+document.getElementById('advanced_search').parentNode.insertAdjacentHTML('beforebegin', '<b><a href="/">Accueil</a></b>&nbsp;&nbsp;');
 
 
 /* Ce déconnecter de la personne 
 
 document.getElementById('current_person').insertAdjacentHTML('beforeend', '<div><a href="/" id="deleted_cookie">Changer de pesronne</a></div> ');*/
 
+
+/* Enleve le popin sur le lien "Les présents" */
+
+var el = document.getElementById('les_presents');
+elClone = el.cloneNode(true);
+elClone.innerHTML = 'Mes usagers (les présents)';
+el.parentNode.replaceChild(elClone, el);
+
+
+/*  Corrige une faute dans le titre "Les Presents" */
+
+if (window.location.pathname == '/les_presents/new') {
+    document.querySelector('#lign_one h1').innerHTML = 'Mes usagers (les présents)';
+}
+
+/* Enleve le popin sur la loupe */
+
+var elements = document.getElementsByClassName('glimpse');
+var elements_user = Array.prototype.filter.call(elements, function(element) {
+    var parser = document.createElement('a');
+    parser.href = element.href;
+    return parser.pathname.substring(0, 8) == "/people/";
+});
+for (var i = 0; i < elements_user.length; i++) { 
+    delete_all_event(elements_user[i]);
+}
+
+function delete_all_event (el) {
+    elClone = el.cloneNode(true);
+    el.parentNode.replaceChild(elClone, el);
+}
+
+
+/* Suppression des target="_blank" */
+
+Array.from(document.querySelectorAll('a[target="_blank"]'))
+  .forEach(link => link.removeAttribute('target'));
 
 
